87ee6e08230e606c6c0f623e3910c3a3f4e9de17
Steve Roche <steve.roche.xyz@proton.me>
2025-01-29 19:45:03 +0530

    Desktop configuration

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # nix-config
+
+```bash
+$ nix-env -i git
+$ git clone https://github.com/SteveRoche/nix-config $HOME/nix-config
+$ sudo rm -rf /etc/nixos/*
+$ sudo ln -s $HOME/nix-config/hosts/desktop/configuration.nix /etc/nixos/configuration.nix
+$ sudo nixos-rebuild switch
+```
---
+++ hosts/desktop/configuration.nix
@@ -0,0 +1,209 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ../../common-configuration.nix
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelParams = [ "nvidia_drm.fbdev=1" ];
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kolkata";
+  time.hardwareClockInLocalTime = true;
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IN";
+    LC_IDENTIFICATION = "en_IN";
+    LC_MEASUREMENT = "en_IN";
+    LC_MONETARY = "en_IN";
+    LC_NAME = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_PAPER = "en_IN";
+    LC_TELEPHONE = "en_IN";
+    LC_TIME = "en_IN";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.graphics.enable = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  users.users.steve = {
+    isNormalUser = true;
+    description = "Steve Roche";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [ ];
+  };
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "steve";
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
+  programs = {
+    firefox.enable = true;
+    zsh.enable = true;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+  
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than +3";
+  };
+  nix.settings.auto-optimise-store = true;
+
+  environment.systemPackages = with pkgs; [
+     efibootmgr
+     nvtopPackages.nvidia
+     cudaPackages.cuda_nvml_dev
+
+     neovim
+     helix
+     wget
+     gitAndTools.gitFull
+     lazygit
+     lf
+     zig
+     zls
+     go
+     gopls
+     lldb_18
+     nodejs_22
+     bun
+     shaderc
+     jq
+     ripgrep
+     tree
+     cloc
+     just
+     zoxide
+     unixtools.watch
+     gh
+     pdftk
+     ghostscript
+
+     gnomeExtensions.pop-shell
+
+     ghostty
+     obsidian
+     vscode-fhs
+     proton-pass
+     protonvpn-gui
+     protonmail-desktop
+     ticktick
+     evince
+
+     inconsolata
+  ];
+
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+    gnome-user-docs
+    gnome-secrets
+    gnome-weather
+    gnome-maps
+    gnome-music
+    gnome-text-editor
+    gnome-console
+    simple-scan
+    totem
+    baobab
+    yelp
+    epiphany
+    geary
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
